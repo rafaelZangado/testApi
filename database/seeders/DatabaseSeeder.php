3ab249507c1894133028040dbc821150dc2b4608
Rafael Andradea <rafael.andrade@vcodes.io>
2023-06-29 14:55:03 -0300

    :sparkles: criacao dos usuarios fakes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,29 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
+    { 
+        //Add grupo Admin
         \App\Models\User::factory()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt(123456),
+                'name' => 'Admin User',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt(123456),
+                'nivel' => 1,
         ]);
+
+        //Add grupo Financeiro
+        \App\Models\User::factory()->create([
+            'name' => 'Financeiro',
+            'email' => 'financeiro@gmail.com',
+            'password' => bcrypt(123456),
+            'nivel' => 2
+        ]);
+
+        //Add grupo Modelador
+        \App\Models\User::factory()->create([
+            'name' => 'Modelador',
+            'email' => 'modelador@gmail.com',
+            'password' => bcrypt(123456),
+            'nivel' => 3
+        ]);           
     }
 }

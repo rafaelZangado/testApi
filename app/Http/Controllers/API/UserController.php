fb15fc960515287c059261682f0ea241e79df881
Rafael Andradea <rafael.andrade@vcodes.io>
2023-06-29 14:53:11 -0300

    :sparkles: criacao do crud

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
+use App\Models\Products;
 use App\Models\User;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
@@ -23,36 +25,60 @@ class UserController extends Controller
         return Response(['status' => 200, 'token' => $token], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+   
     public function getUserDetail()
     {
         $user = Auth::guard('api')->user();
         return Response(['Data' => $user],200);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
+   
+    public function addItem(ProductRequest $request)
     {
-        //
+        $validatedData = $request->validated();
+        $product = Products::create($validatedData);
+        return response()->json($product, 201);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
+   
+    public function listarItens()
     {
-        //
+        $products = Products::all();
+        return response()->json($products);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
+    public function editarItem(Request $request)
     {
-        //
+        $product = Products::find($request->id);
+       
+        if (!$product) {
+            return response()->json([
+                'error' => 'Produto não encontrado'
+            ], 404);
+        }
+
+      
+        $product->name = $request->input('name');
+        $product->phone = $request->input('phone');
+
+        
+        $product->save();
+
+        
+        return response()->json([
+            'message' => 'Produto atualizado com sucesso', 
+            'data' => $product
+        ], 200);
+
+    }
+
+    public function deletarItem(Request $request)
+    {
+        $product = Products::findOrFail($request->id);
+        $product->delete();
+
+        return response()->json([
+            'message' => 'Produto excluído com sucesso'
+        ]);
     }
 }
